f2f5bfb668c248361540ec2a63591b8a9c136219
GravityIO <Harryioannoucy@gmail.com>
2026-07-04 14:02:16 +0300

    forgot the balls in

--- scripts/LocaleKeys.lua
+++ scripts/LocaleKeys.lua
@@ -1,9 +1,6 @@
 -- Auto-generated from config
 local LocaleKeys = {
   STALKER = {
-    RANDOM = {
-      BALLS = "stalker.random.balls",
-    },
     MESSAGES = {
       ALONE = "stalker.messages.alone",
       NOW_STALKING = "stalker.messages.now_stalking",
